3de6f87a63706f5701e1ce14222b8407ade237f3
Przemyslaw Grenda <przemekgrenda@gmail.com>
2022-07-04 09:03:20 +0200

    Main page layout basics with tailwind.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,62 @@
 import Head from 'next/head';
-
+import Image from 'next/image'
+import picOfMe from '../../public/static/me.jpg'
 import { getAllPosts } from '../lib/api';
 import Link from '../components/Link';
 
 const Index = ({ posts }) => {
   return (
-    <main>
+    <main class="flex flex-col">
       <Head>
         <title>{'Neverending spiral'}</title>
       </Head>
-      <h1>{'Neverending spiral:'}</h1>
+      <div class="flex flex-row bg-indigo-500 content-between">
+          <div class="flex flex-row my-2">
+            <div>
+              <a href="">
+                <span class="text-2xl mx-8">Home</span>
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <span class="text-2xl mx-8">Notes</span>
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <span class="text-2xl mx-8">Blog</span>
+              </a>
+            </div>
+          </div>
+        <div class="flex-1  bg-yellow-500">
+          {/* TODO add theme switch */}
+          {/* https://heroicons.com/ */}
+        </div>
+
+</div>
+      <div class="mx-4 mt-8">
+        <div class="flex flex-row">
+          <div>
+            <p class="text-4xl my-2">Welcome!</p>
+            <p class="text-3xl">My name is</p>
+            <p class="text-3xl font-bold">Przemysław Grenda</p>
+            <p class="text-1xl my-2 italic">This is the place where my ideas grow</p>
+          </div>
+          <div>
+          <Image class="rounded-full"
+            src={picOfMe}
+      alt="Picture of the author"
+      width={200}
+      height={200}
+    />
+          </div>
+
+        </div>
+
+
+
+
+      <h1 class="text-lg">Test</h1>
       <ul>
         {posts.map((p) => (
           <li key={p.path}>
@@ -17,6 +64,7 @@ const Index = ({ posts }) => {
           </li>
         ))}
       </ul>
+    </div>
     </main>
   );
 };
