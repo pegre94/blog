1c4e459c91c0671f4074e50c2a8fe5eede10a537
Przemyslaw Grenda <przemekgrenda@gmail.com>
2022-07-21 18:58:28 +0200

    vercel submodule-workaround fix

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -1,8 +1,8 @@
 # github submodule repo address without https:// prefix
-SUBMODULE_GITHUB=github.com/beeinger/vercel-private-submodule
+SUBMODULE_GITHUB=github.com/pegre94/blog
 
 # .gitmodules submodule path
-SUBMODULE_PATH=library
+SUBMODULE_PATH=./public/roam
 
 # github access token is necessary
 # add it to Environment Variables on Vercel
