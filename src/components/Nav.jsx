24db801ab17a73e9532970a2c741b5cc8e173ccc
Przemyslaw Grenda <przemekgrenda@gmail.com>
2022-11-01 20:53:33 +0100

    search beta

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,27 +18,31 @@ const searchVisible = () => {
 
 const TopNav = ({ onFocus, onBlur }) => {
   return (
-    <div className="flex flex-row my-2 items-center justify-between">
-      <nav class="flex-row ">
-        {[['Home', '/'],
-        ['About', '/about']].map(([title, url]) => (
-          <a key={title} href={url} className={"rounded-lg px-4 py-2 mr-5 text-slate-700 font-semibold hover:bg-slate-300  hover:text-slate-900" +
-            (isActive(url) === true ? " bg-slate-200" : "")} > {title}</a>
-        ))}
-      </nav >
-      {(searchVisible() === true ?
-        <div className="flex">
-          <form class="flex items-center">
-            <div class="relative w-fit">
-              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+    <div >
+      <nav className="relavtive flex mt-2 items-center justify-between">
+        <div className="flex flex-nowrap">
+          {[['Home', '/'],
+          ['About', '/about']].map(([title, url]) => (
+            <a key={title} href={url} className={"rounded-lg px-4 py-2 sm:mr-5 text-slate-700 font-semibold hover:bg-slate-300  hover:text-slate-900" +
+              (isActive(url) === true ? " bg-slate-200" : "")} > {title}</a>
+          ))}
+        </div>
+        {(searchVisible() === true ?
+          <div>
+            <form className="flex items-center ">
+              <div className="relative">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </div>
+                <input type="text" onFocus={onFocus} onBlur={onBlur} id="search" className="bg-gray-50 py-2 border sm:w-full border-gray-300 text-gray-900 text-sm rounded-lg focus:slate-300 focus:border-slate-300 w-48 pl-10 p-2.5" placeholder="Search.." />
               </div>
-              <input type="text" onFocus={onFocus} onBlur={onBlur} id="search" class="bg-gray-50 py-2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:slate-300 focus:border-slate-300  pl-10 p-2.5" placeholder="Search notes" required />
-            </div>
-          </form>
-        </div >
-        : null)}
-    </div >
+            </form>
+          </div>
+
+          : null)}
+      </nav >
+    </div>
+
 
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ import TopNav from "../components/Nav";
 import { unified } from 'unified';
 import rehype2react from 'rehype-react';
 import Link from "../components/Link";
-import React from 'react';
+import React, { Component } from 'react';
 
 const DummyLink = ({ href, ...props }) => {
   return (
@@ -26,75 +26,103 @@ const processor = unified()
       a: DummyLink,
     }
   });
+
+
+
+class MovableBlock extends Component {
+  render() {
+
+    return (
+      <div>
+        <p className="text-1xl sm:my-2 italic">
+          and this is the place where my ideas grow
+        </p>
+        <div class="flex justify-center pt-2">
+          <div class="flex sm:mt-4 space-x-6">
+            <a href="mailto:przemekgrenda@gmail.com" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>
+              <span class="sr-only">Mail page</span>
+            </a>
+            <a href="https://github.com/pegre94/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" /></svg>
+              <span class="sr-only">GitHub account</span>
+            </a>
+            <a href="https://www.linkedin.com/in/przemyslaw-grenda/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-400 dark:hover:text-gray-600">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+              <span class="sr-only">Linkedin account</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 function About({ focus }) {
   if (focus === false) {
     return (
-      <div>
-        <div className="mt-8 mx-3">
-          <div className="flex flex-row justify-between">
-            <div className="w-3/5">
-              <div className="inline-block">
-                <p className="text-3xl mb-2">Welcome!</p>
-                <p className="text-2xl">My name is</p>
-                <p className="text-3xl font-bold">Przemysław Grenda</p>
-                <p className="text-1xl my-2 italic">
-                  and this is the place where my ideas grow
-                </p>
-                <div class="flex justify-center pt-2">
-                  <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                    <a href="mailto:przemekgrenda@gmail.com" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>
-                      <span class="sr-only">Mail page</span>
-                    </a>
-                    <a href="https://github.com/pegre94/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" /></svg>
-                      <span class="sr-only">GitHub account</span>
-                    </a>
-                    <a href="https://www.linkedin.com/in/przemyslaw-grenda/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-400 dark:hover:text-gray-600">
-                      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
-                      <span class="sr-only">Linkedin account</span>
-                    </a>
-                  </div>
-                </div>
+      <div className="mt-8 mx-3">
+        <div className="flex flex-row justify-between">
+          <div className="3/5">
+            <div className="inline-block ">
+              <p className="text-3xl mb-2">Welcome!</p>
+              <p className="text-2xl">My name is</p>
+              <p className="text-3xl font-bold">Przemysław Grenda</p>
+              <div className="hidden sm:block">
+                <MovableBlock />
               </div>
-            </div>
-            <div className="w-2/5">
-              <Image
-                className="rounded-full "
-                src={picOfMe}
-                alt="Picture of the author"
-              />
+
             </div>
           </div>
-        </div >
-        <h1 className="text-xl mb-4">Recently updated notes:</h1>
-      </div>
+          <div className="w-2/5">
+            <Image
+              className="rounded-full "
+              src={picOfMe}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
+        <div className="sm:hidden place-content-center ">
+          <MovableBlock />
+        </div>
+        <h1 className="text-xl mt-2 mb-2">Recently updated notes:</h1>
+      </div >
     )
   } else {
-    return null
+    return (
+      <h1 className="text-xl my-4">Search results:</h1>
+
+
+    )
   }
 }
-const Index = ({ posts }) => {
+const Index = ({ posts, recentPosts }) => {
+
 
   const [focus, setFocus] = React.useState(false);
+  const [currentPosts, setCurrentPosts] = React.useState(recentPosts);
   const onFocus = () => {
     setFocus(true)
-
+    setCurrentPosts(posts);
   }
   const onBlur = () => {
-    setFocus(false)
+    // sleep for 100ms to wait for the search to finish
+    setTimeout(() => {
+      setFocus(false)
+      setCurrentPosts(recentPosts);
+    }, 100);
   }
   return (
-    <main className="flex flex-col ">
+    <main className="flex flex-col">
       <Head>
         <title>{"Neverending spiral"}</title>
       </Head>
-      <TopNav onFocus={onFocus} onBlur={onBlur} />
+      <TopNav onFocus={onFocus} onBlur={onBlur} className="inline-block" />
       <About focus={focus} />
 
-      <div className="">
+      <div >
         <ul className="list-disc">
-          {posts.map((post) => (
+          {currentPosts.map((post) => (
             <Link href={post.path}>
               <div className="mb-1 w-full lg:max border border-gray-300 rounded hover:bg-slate-100">
                 <div className="space-y-2">
@@ -151,8 +179,8 @@ export const getStaticProps = async () => {
   // TODO: refactor there is alredy post value here and we are droping it to obtain it again later
   let posts = allPosts
     .map((p) => ({ title: p.data.title || p.basename, path: p.path, mtime: p.data.mtime, tags: p.data.filetags?.slice(1, -1).split(":") || [] }))
-    .slice(0, 5)
-    .sort((a, b) => new moment(a.mtime).format('YYYYMMDDHHmmss') - new moment(b.mtime).format('YYYYMMDDHHmmss'))
+    // sort by date with moment
+    .sort((a, b) => moment(b.mtime, 'YYYYMMDDHHmmss').diff(moment(a.mtime, 'YYYYMMDDHHmmss')))
 
   posts = await Promise.all(posts.map(async (p) => {
     const post = await getPostBySlug(p.path);
@@ -161,8 +189,9 @@ export const getStaticProps = async () => {
     const numWords = getWords(postText)
     const readingTime = getReadingTime(postText)
 
-
     return { ...p, hast: post.result, numWords: numWords, readingTime: readingTime }
   }))
-  return { props: { posts } };
+  let recentPosts = posts.slice(0, 3)
+
+  return { props: { posts, recentPosts } };
 };
